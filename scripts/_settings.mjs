839aa7bd53dd4ac127f336863f5aad1671dff1d2
Robert McIntosh <rmcintosh@remabledesigns.com>
2022-09-19 12:01:31 -0400

    v2.0.2a
    Added Support for lib - Server Settings

--- scripts/_settings.mjs
+++ scripts/_settings.mjs
@@ -4,6 +4,12 @@ import { MODULE } from './_module.mjs';
 // FOUNDRY HOOKS -> SETUP
 Hooks.once('setup', () => {
 	// SET MODULE MIGRATE SETTINGS
+	MODULE.setting('register', 'enableGlobalConflicts', {
+		type: Boolean,
+		default: true,
+		config: false,
+		scope: 'world',
+	});
 	MODULE.setting('register', 'clientMigratedVersion', {
 		type: String,
 		default: "0.0.0",
@@ -22,15 +28,27 @@ Hooks.once('setup', () => {
 		scope: 'world',
 		config: false
 	});
+	MODULE.setting('register', 'renamedModules', {
+		type: Object,
+		default: {},
+		scope: 'world',
+		config: false
+	});
 
 	// SET MODULE SETTINGS
-	
-	MODULE.setting('register', 'trackedChangelogs', {
+	const trackedChangelogs =  {
 		type: Object,
 		default: {},
 		config: false,
-		scope: 'client'
-	});
+	}
+	if (game.modules.get('lib-server-setting')?.active ?? false) {
+		Hooks.once('lib-server-setting.Setup', async (SETTING) => {
+			SETTING(MODULE.ID, 'trackedChangelogs', trackedChangelogs);
+		});
+	}else{
+		MODULE.setting('register', 'trackedChangelogs', trackedChangelogs);
+	}
+
 	MODULE.setting('register', 'presets', {
 		type: Object,
 		default: {},
@@ -45,12 +63,6 @@ Hooks.once('setup', () => {
 	MODULE.setting('register', 'showNewChangelogsOnLoad', {
 		type: Boolean,
 		default: true,
-		scope: 'world',
-	});
-	MODULE.setting('register', 'enableGlobalConflicts', {
-		type: Boolean,
-		default: true,
-		config: false,
 		scope: 'world',
 	});
 	MODULE.setting('register', 'bigPictureMode', {
